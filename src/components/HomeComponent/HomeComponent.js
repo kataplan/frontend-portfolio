3dba2cf922a6cb1be6a5a4da53686f0d2bcf4fde
Kataplan <dcatalanmarchese@gmail.com>
2022-02-10 18:24:26 -0300

    Add HomeComponent

---
+++ src/components/HomeComponent/HomeComponent.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'home-component',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/HomeComponent/index.vue
@@ -0,0 +1,4 @@
+<template src="./HomeComponent.html"></template>
+<script src="./HomeComponent.js"></script>
+<style src="./HomeComponent.scss" scoped lang="scss"></style>
+
