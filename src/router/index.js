c578f6eb45b8750ab79c447765a119962bb7f25c
Kataplan <dcatalanmarchese@gmail.com>
2022-02-11 17:26:41 -0300

    Add routes to important pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Contact from "../views/Contact.vue";
+import Proyects from "../views/Proyects.vue";
+import About from "../views/About.vue";
 
 const routes = [
   {
@@ -8,13 +11,19 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/contactame",
+    name: "contact",
+    component: Contact,
+  },
+  {
+    path: "/acerca-de-mi",
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/proyectos",
+    name: "proyects",
+    component: Proyects,
   },
 ];
 
