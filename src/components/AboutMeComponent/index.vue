69f51827b2309d00a26c91ec3ed372db9c4af4aa
Kataplan <dcatalanmarchese@gmail.com>
2022-02-11 17:00:21 -0300

    Delete unused components

--- src/components/AboutMeComponent/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./AboutMeComponent.html"></template>
-<script src="./AboutMeComponent.js"></script>
-<style src="./AboutMeComponent.scss" scoped lang="scss"></style>
-
--- src/components/HomeComponent/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./HomeComponent.html"></template>
-<script src="./HomeComponent.js"></script>
-<style src="./HomeComponent.scss" scoped lang="scss"></style>
-
