fc993080ad8dee8f0959048313a6cc1a22b7a0f9
Kataplan <dcatalanmarchese@gmail.com>
2022-03-03 17:45:27 -0300

    Config vue proyect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
     <div>
       <h2>Diego Catalán</h2>
     </div>
-    
     <div>
       Soy estudiante de Ingeniería Civil Informática, programador frontend y con
       meta de llegar a dominar el fullstack
--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,10 @@
-const path = require("path");
-
 module.exports = {
-  chainWebpack(config) {
-    config.entry("app").clear().add("./src/core/main.js").end();
-    config.resolve.alias
-      .set("~", path.join(__dirname, "./src"))
-      .set("@", path.join(__dirname, "./src/core"))
-      .set("#", path.join(__dirname, "./src/modules"));
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        sassOptions: {
-          includePaths: [path.resolve(__dirname, "src/core/")],
-          indentedSyntax: true,
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: '@import "~@/styles/base.scss";',
+            },
         },
-        prependData: '@import "~@/assets/sass/main.scss"',
-      },
     },
-  },
-  assetsDir: "@/assets/",
-};
+    assetsDir: '@/assets/',
+}
