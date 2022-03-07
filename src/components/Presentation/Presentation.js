e795abc7ed91e97d2e4311453a529edc1daf297b
Kataplan <dcatalanmarchese@gmail.com>
2022-03-06 21:24:06 -0300

    Add Presentation object

---
+++ src/components/Presentation/Presentation.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'presentation',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/Presentation/index.vue
@@ -0,0 +1,4 @@
+<template src="./Presentation.html"></template>
+<script src="./Presentation.js"></script>
+<style src="./Presentation.scss" scoped lang="scss"></style>
+
