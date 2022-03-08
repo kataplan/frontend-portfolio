7ff219f8861fa13a0cd898119885ea40bceaeeb3
Kataplan <dcatalanmarchese@gmail.com>
2022-03-07 23:18:33 -0300

    Add Aptitudes component

---
+++ src/components/Aptitudes/Aptitudes.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'aptitudes',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/Aptitudes/index.vue
@@ -0,0 +1,4 @@
+<template src="./Aptitudes.html"></template>
+<script src="./Aptitudes.js"></script>
+<style src="./Aptitudes.scss" scoped lang="scss"></style>
+
