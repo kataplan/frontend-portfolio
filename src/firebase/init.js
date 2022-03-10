3e207adf58c015a3a330d8859e87022d2a9bfcda
Kataplan <dcatalanmarchese@gmail.com>
2022-03-10 18:22:18 -0300

    Update firebase init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,7 +1,6 @@
-
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQBXVZPHoQtWJUzTgQdwGsYC5iDyzOsrM",
@@ -10,11 +9,11 @@ const firebaseConfig = {
   storageBucket: "portafolio-backend-61b0e.appspot.com",
   messagingSenderId: "108650769890",
   appId: "1:108650769890:web:00a7e732bee02f5f3b9dbb",
-  measurementId: "G-5QVXKS1K8P"
+  measurementId: "G-5QVXKS1K8P",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore()
-export default db
+export const storage = getStorage(app);
+const db = getFirestore();
+export default db;
