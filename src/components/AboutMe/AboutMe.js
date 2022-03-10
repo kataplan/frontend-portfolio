b6ac416a4258a6ea8e07d3352f4eca9b9b5f6c71
Kataplan <dcatalanmarchese@gmail.com>
2022-03-10 14:40:48 -0300

    Add AboutMe component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+export default {
+  name: "about-me",
+  components: {},
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
---
+++ src/components/AboutMe/index.vue
@@ -0,0 +1,4 @@
+<template src="./AboutMe.html"></template>
+<script src="./AboutMe.js"></script>
+<style src="./AboutMe.scss" scoped lang="scss"></style>
+
