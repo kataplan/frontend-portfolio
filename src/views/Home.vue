c241782cf71844b2d8d158ac5b8fc16c2d54b29c
Kataplan <dcatalanmarchese@gmail.com>
2022-03-10 14:42:06 -0300

    Upadte Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="home">
     <Presentation></Presentation>
     <Aptitudes></Aptitudes>
+    <AboutMe></AboutMe>
   </div>
 </template>
 
 <script>
 import Presentation from "@/components/Presentation";
 import Aptitudes from "@/components/Aptitudes"
+import AboutMe from "@/components/AboutMe"
 
 export default {
   name: "Home",
-  components: {Presentation,Aptitudes},
+  components: {Presentation,Aptitudes,AboutMe},
 };
 </script>
 
