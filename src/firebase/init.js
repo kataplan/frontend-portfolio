6e3f384d45786c497ee2df01f637f62326418460
Kataplan <dcatalanmarchese@gmail.com>
2022-03-11 09:06:43 -0300

    Update firebase init

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
