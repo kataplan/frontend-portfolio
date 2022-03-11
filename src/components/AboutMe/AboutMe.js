b57788b6b0b6d880ce1e170ed3cb4d799be430f7
Kataplan <dcatalanmarchese@gmail.com>
2022-03-11 09:06:26 -0300

    Update AboutMe component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,3 +1,6 @@
+import { storage } from "@/firebase/init";
+import { ref, getDownloadURL } from "firebase/storage";
+
 export default {
   name: "about-me",
   components: {},
@@ -7,5 +10,27 @@ export default {
   },
   computed: {},
   mounted() {},
-  methods: {},
+  methods: {
+    handleDownload() {
+      const gsReference = ref(storage, "Diego-Catalan-CV.pdf");
+      getDownloadURL(gsReference)
+        .then((url) => {
+          window.open(url);
+        })
+        .catch((error) => {
+          switch (error.code) {
+            case "storage/object-not-found":
+              break;
+            case "storage/unauthorized":
+              break;
+            case "storage/canceled":
+              // User canceled  upload
+              break;
+
+            case "storage/unknown":
+              break;
+          }
+        });
+    },
+  },
 };
