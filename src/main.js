855969e04b36cd4aa83e8e072834bcbf62cc872c
Kataplan <dcatalanmarchese@gmail.com>
2022-03-24 00:03:04 -0300

    Import fontawesome

--- src/main.js
+++ src/main.js
@@ -2,5 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+const app = createApp(App);
 
-createApp(App).use(store).use(router).mount("#app");
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+
+app.use(store).use(router).mount("#app");
