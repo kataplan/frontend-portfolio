10ae5b78693d1ab92b8462d472293bb2b6e6e206
Kataplan <dcatalanmarchese@gmail.com>
2022-05-26 00:05:06 -0400

    Update Slider

--- src/components/ImageSlider/Slider.js
+++ src/components/ImageSlider/Slider.js
@@ -2,18 +2,18 @@
 export default {
   name: 'slider',
   components: {},
-  props: {images:Array},
-  data () {
+  props: { images: Array },
+  data() {
     return {
-      slideIndex:1,
+      slideIndex: 1,
     }
   },
   computed: {
 
   },
-  mounted () {
+  mounted() {
     this.showSlides(1)
-    
+
   },
   methods: {
     plusSlides(n) {
@@ -22,13 +22,13 @@ export default {
     showSlides(n) {
       let i;
       let slides = document.getElementsByClassName("slider_slide");
-      if (n > slides.length) {this.slideIndex = 1}
-      if (n < 1) {this.slideIndex = slides.length}
+      if (n > slides.length) { this.slideIndex = 1 }
+      if (n < 1) { this.slideIndex = slides.length }
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
-    
-      slides[this.slideIndex-1].style.display = "block";
+
+      slides[this.slideIndex - 1].style.display = "block";
     }
   }
 }
