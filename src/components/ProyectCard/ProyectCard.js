ce631fe1c61923c8eb0ca5d9ef4ce4797ed0a527
Kataplan <dcatalanmarchese@gmail.com>
2022-05-28 18:36:28 -0400

    Update ProyectCard

--- src/components/ProyectCard/ProyectCard.js
+++ src/components/ProyectCard/ProyectCard.js
@@ -1,6 +1,5 @@
 
-import { ref, getDownloadURL } from "firebase/storage";
-import { storage } from "@/firebase/init";
+
 import ProyectDetails from "@/components/ProyectDetails"
 export default {
 
@@ -9,51 +8,16 @@ export default {
   props: { proyect: Object },
   data() {
     return {
-      imagesUrl: [],
-      thumbnail: String,
       open: false,
-      isLoaded: false,
     }
   },
   computed: {
 
   },
   mounted() {
-    this.imagesUrl = []
-    this.getImages()
   },
   methods: {
-    async getImages() {
-      this.handleThumbnail("thumbnails/" + this.proyect.thumbnail._key.path.segments[8])
-
-    },
-    handleThumbnail(reference) {
-      const gsReference = ref(storage, reference);
-      getDownloadURL(gsReference)
-        .then((url) => {
-          this.thumbnail = url;
-          this.isLoaded = true;
-        })
-        .catch((error) => {
-          switch (error.code) {
-            case "storage/object-not-found":
-              break;
-            case "storage/unauthorized":
-              break;
-            case "storage/canceled":
-              // User canceled  upload
-              break;
-
-            case "storage/unknown":
-              break;
-          }
-        });
-
-    },
-    getImgUrl(str) {
-      var images = require.context('@/assets/icons', false, /\.svg$/)
-      return images('./' + str.toLowerCase() + ".svg")
-    },
+   
     handleClose(){
       this.open = false;
     }
