c2b42d1f17325b62dcb62085ce4e5cb46e6c1c19
Kataplan <dcatalanmarchese@gmail.com>
2022-11-14 22:10:48 -0300

    Fix proyect css

--- src/views/Proyects.vue
+++ src/views/Proyects.vue
@@ -2,12 +2,13 @@
   <section class="proyects">
     <h1 class="section-title">Mis Proyectos</h1>
     <p class="proyects_text">Aquí se muestran los proyectos y trabajos que he realizado y he sido parte.</p>
-    <div class="input-container proyects_input-container">
-      <input type="text" @keyup="onChange($event)" class="input" placeholder="Realiza una búsqueda"/>
-      <span class="label">Realiza una búsqueda</span>
+    <div class="input-container proyects_input-container group ">
+      <input type="text" @keyup="onChange($event)" class="input" placeholder="Realiza una búsqueda" required />
+      <label for="input" class="control-label">Escriba aquí</label>
       <i class="fa-solid fa-magnifying-glass proyects_search-icon"></i>
+
     </div>
-    
+
     <div class="proyects_display">
       <TransitionGroup name="fade">
         <ProyectCard v-for="(item, index) in filteredProyects" :key="index" :proyect="item"></ProyectCard>
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-import { db,storage } from "@/firebase/init";
+import { db, storage } from "@/firebase/init";
 import { ref, getDownloadURL } from "firebase/storage";
 import { collection, getDocs } from "firebase/firestore";
 import ProyectCard from "@/components/ProyectCard";
@@ -27,23 +28,23 @@ export default {
   components: { ProyectCard },
 
   data() {
-    return { proyects: [], filteredProyects:[]};
+    return { proyects: [], filteredProyects: [] };
   },
 
   methods: {
-    onChange(e){
+    onChange(e) {
       const search = e.target.value.toLowerCase();
-      this.filteredProyects = this.proyects.filter((item)=> {
-        if(item.title.toLowerCase().includes(search)) {
+      this.filteredProyects = this.proyects.filter((item) => {
+        if (item.title.toLowerCase().includes(search)) {
           return item;
-        }else{
+        } else {
           let isIncluded = false
-          item.stack.forEach((stck)=>{
-            if(stck.toLowerCase().includes(search)) {
+          item.stack.forEach((stck) => {
+            if (stck.toLowerCase().includes(search)) {
               isIncluded = true;
             }
           })
-          if(isIncluded){
+          if (isIncluded) {
             return item
           }
         }
@@ -60,19 +61,19 @@ export default {
       });
     },
     async getImages(proyect) {
-      this.handleThumbnail("thumbnails/" + proyect.thumbnail._key.path.segments[8],proyect)
+      this.handleThumbnail("thumbnails/" + proyect.thumbnail._key.path.segments[8], proyect)
     },
-    sortArray(array){
-      array.sort((a,b) => {
+    sortArray(array) {
+      array.sort((a, b) => {
         let fa = a.title.toLowerCase(),
-        fb = b.title.toLowerCase();
-        if (fa < fb) {return -1;}
-        if (fa > fb) {return 1;}
+          fb = b.title.toLowerCase();
+        if (fa < fb) { return -1; }
+        if (fa > fb) { return 1; }
         return 0;
       })
-      
+
     },
-    handleThumbnail(reference,proyect) {
+    handleThumbnail(reference, proyect) {
       const gsReference = ref(storage, reference);
       getDownloadURL(gsReference)
         .then((url) => {
@@ -80,7 +81,7 @@ export default {
           this.proyects.push(proyect);
           this.filteredProyects.push(proyect);
           this.sortArray(this.filteredProyects)
-          })
+        })
         .catch((error) => {
           switch (error.code) {
             case "storage/object-not-found":
@@ -104,12 +105,62 @@ export default {
 </script>
 
 <style lang="scss">
+.group {
+  position: relative;
+
+  padding: 5px 15px;
+  border: 3px solid var(--text-color);
+  border-radius: 20px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: var(--body-fg);
+  height: 50px;
+
+}
+
+
+
+.input {
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  background: none;
+  color: var(--text-color);
+
+}
+
+/* LABEL ======================================= */
+label {
+  color: #999;
+  font-size: 18px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 0.2s ease all;
+  -moz-transition: 0.2s ease all;
+  -webkit-transition: 0.2s ease all;
+}
+
+/* active state */
+.input:focus~label,
+.input:valid~label {
+  top: -22px;
+  font-size: 14px;
+  color: var(--accent);
+}
 .proyects {
   width: 100%;
   min-height: 100vh;
   height: fit-content;
   padding: 84px 5%;
   box-sizing: border-box;
+
   &_display {
     margin-top: 50px;
     display: grid;
@@ -117,38 +168,47 @@ export default {
     grid-template-columns: repeat(auto-fit, minmax(250px, 0.5fr));
     grid-gap: 15px;
     box-sizing: border-box;
+    align-items: center;
 
   }
-  &_text{
+
+  &_text {
     font-size: 1rem;
-    margin-bottom:2rem ;
+    margin-bottom: 2rem;
   }
-  &_input-container{
-      width: 20%;
-      min-width: 300px;
+
+  &_input-container {
+    width: 100%;
+    min-width: 300px;
+    max-width: 500px;
   }
-  &_search-icon{
+
+  &_search-icon {
     color: var(--decor)
   }
+
   @media (max-width: 980px) {
-      padding: 10px 5% 84px;
+    padding: 10px 5% 84px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &_display {
       display: flex;
       flex-direction: column;
-      align-items: center;
-      &_display{
-        display: flex;
-        flex-direction: column;
-        width: 90%;
-        max-width: 600px;
-      }
-      &_text{
-        width: 90%;
-        text-align: center;
-        margin-top: 0.9rem;
-      }
-      &_input-container{
-        width: 90%;
-      }
+      width: 90%;
+      max-width: 600px;
     }
+
+    &_text {
+      width: 90%;
+      text-align: center;
+      margin-top: 0.9rem;
+    }
+
+    &_input-container {
+      width: 90%;
+    }
+  }
 }
 </style>
