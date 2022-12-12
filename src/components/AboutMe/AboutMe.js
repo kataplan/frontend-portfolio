33cb7a701f17bcb9b9792140f1ea093c4de968fb
Kataplan <dcatalanmarchese@gmail.com>
2022-12-12 17:57:39 -0300

    Update AboutMe.js animations

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,9 @@
 import { storage } from "@/firebase/init";
 import { ref, getDownloadURL } from "firebase/storage";
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default {
   name: "about-me",
@@ -7,6 +11,26 @@ export default {
   props: [],
   data() {
     return {};
+  },
+  setup(){
+    const beforeEnter =(el) =>{
+      el.style.transform =  'translateX(20px)'
+      el.style.opacity = 0
+    }
+    const enter =(el,done) =>{
+      gsap.to(el,{
+        scrollTrigger: el,
+        duration:1,
+        y:0,
+        opacity:1,
+        ease: 'circ.out',
+        onComplete:done,
+        rotate:0,
+        translateX:0,
+        delay: el.dataset.index*0.6
+      })
+    }
+    return {beforeEnter, enter}
   },
   computed: {},
   mounted() {},
