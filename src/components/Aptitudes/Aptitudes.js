c8586b8bff8765aa728ba095de32588f1d5ad799
Kataplan <dcatalanmarchese@gmail.com>
2022-12-12 17:57:07 -0300

    Update animation to Aptitudes

--- src/components/Aptitudes/Aptitudes.js
+++ src/components/Aptitudes/Aptitudes.js
@@ -1,4 +1,8 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 export default {
   name: 'aptitudes',
   components: {},
@@ -7,6 +11,24 @@ export default {
     return {
 
     }
+  },
+  setup(){
+    const beforeEnter =(el) =>{
+      el.style.transform = 'translateY(30px)'
+      el.style.opacity = 0
+    }
+    const enter =(el,done) =>{
+      gsap.to(el,{
+        scrollTrigger: el,
+        duration:1,
+        y:0,
+        opacity:1,
+        ease: 'Power0.out',
+        onComplete:done,
+        delay: el.dataset.index*0.5
+      })
+    }
+    return {beforeEnter, enter}
   },
   computed: {
 
