f5716523f83ff44bcedba078bd16902560bfd920
Kataplan <dcatalanmarchese@gmail.com>
2022-12-12 17:58:06 -0300

    Update App.Vue for transtitions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <section>
     <Navbar></Navbar>
-    <router-view></router-view>
+    <router-view v-slot="{Component}">
+      <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+      </transition>
+    </router-view>
   </section>
 </template>
 
-<style lang="scss">
-@import "@/styles/base";
-</style>
 
 <script>
 import Navbar from "@/components/Navbar";
@@ -16,3 +17,26 @@ export default {
   components: { Navbar },
 };
 </script>
+
+<style lang="scss">
+@import "@/styles/base";
+  .route-enter-from{
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  .route-enter-active{
+    transition: all 0.3s ease-out;
+    position: fixed;
+  }
+
+  .route-leave-to{
+    opacity: 0;
+    position: fixed;
+
+    transform: translateX(100px);
+  }
+  .route-leave-active{
+    transition: all 0.3s ease-in;
+    
+  }
+</style>
