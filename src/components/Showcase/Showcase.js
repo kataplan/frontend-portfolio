22d58c998cb4aae4f5afe6022364af947bedaea6
Kataplan <dcatalanmarchese@gmail.com>
2022-12-13 17:24:22 -0300

    Add Showcase component

---
+++ src/components/Showcase/Showcase.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'showcase',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
---
+++ src/components/Showcase/index.vue
@@ -0,0 +1,4 @@
+<template src="./Showcase.html"></template>
+<script src="./Showcase.js"></script>
+<style src="./Showcase.scss" scoped lang="scss"></style>
+
