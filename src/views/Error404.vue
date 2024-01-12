73de4c0b78004d51c860e1034563d05f34c4d9bd
Kataplan <dcatalanmarchese@gmail.com>
2024-01-12 16:41:11 -0300

    update error 404

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="404">
-    <h1>Error 404</h1>
+  <div class="not-found">
+    <h1 class="section-title">Error 404</h1>
+    <h4>
+      <a href="/">Volver a la página de inicio.</a>
+    </h4>
   </div>
 </template>
 
@@ -10,3 +13,14 @@ export default {
   components: {},
 };
 </script>
+<style lang="scss">
+  .not-found {
+    width: 100%;
+    min-height: 100vh;
+    height: fit-content;
+    padding: 84px 5%;
+  }
+  a{
+    color: var(--text-color);
+  }
+</style>
